5b97be9a503006963a6b881befd5b9958442a99e
Pallab Barman <76774117+pallabbarman@users.noreply.github.com>
2021-02-08 13:31:31 +0600

    song name and artist

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# hard-rock-music
+<h1>Hard Rock Music</h1>
+<p><a href="https://pallabbarman.github.io/hard-rock-music/">Live Site</a></p>
--- app.js
+++ app.js
@@ -1,7 +1,26 @@
+// search your favorite songs 
 const searchSongs = () => {
     const searchText = document.getElementById('search-song').nodeValue;
     const url = `https://api.lyrics.ovh/suggest/${searchText}`
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => displaySongs(data.data))
+}
+
+// find song 
+const displaySongs = songs => {
+    const songContainer = document.getElementById('song-container');
+    songs.forEach(song => {
+        const songDetail = document.createElement('div');
+        songDetail.className = 'single-result row align-items-center my-3 p-3';
+        songDetail.innerHTML = `
+        <div class="col-md-9">
+            <h3 class="lyrics-name">${song.title}</h3>
+            <p class="author lead">Album by <span>${song.artist.name}</span></p>
+        </div>
+        <div class="col-md-3 text-md-right text-center">
+            <button class="btn btn-success">Get Lyrics</button>
+        </div>`;
+        songContainer.appendChild(songDetail);
+    });
 }
